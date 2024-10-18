0f369211dfcfc6985e9fb987f78214ed9917199c
Mari <mari@MacBook-Pro.local>
2024-10-18 20:32:42 +0300

    js fix

--- front/js/main.js
+++ front/js/main.js
@@ -6,3 +6,19 @@
     setHeight();
     window.addEventListener('resize', setHeight);
 })();
+
+document.addEventListener("DOMContentLoaded", function() {
+    function isIOS() {
+        return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    }
+
+    function isMacOS() {
+        return navigator.platform === 'MacIntel' && !navigator.userAgent.includes('Mobile');
+    }
+
+    let bannerTitleTop = document.querySelector('.banner__title-top');
+
+    if (!isIOS() && !isMacOS() && bannerTitleTop) {
+        bannerTitleTop.classList.add('mar-pad');
+    }
+});
